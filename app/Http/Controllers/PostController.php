4fb9de15ff1304fbe9684c44bb99803be25e125f
NandhiEsswaran-001 <nandhiesswaransaravanan@gmail.com>
2026-06-28 15:27:12 +0530

    Policy and controller delete post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,15 @@ use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
+    public function delete(Post $post)
+    {
+        if(auth()->user()->cannot('delete', $post)){
+            return 'You cannot do this';
+        }
+        $post->delete();
+        return redirect('/profile/'. auth()->user()->username)->with('success', 'Post successfully deleted!');
+    }
+
     public function viewSinglePost(Post $post) {
         $post['body'] = strip_tags(Str::markdown($post->body), "<p><ol><ul><li><strong><h3><em>");
         return view('single-post', ['post' => $post]);
